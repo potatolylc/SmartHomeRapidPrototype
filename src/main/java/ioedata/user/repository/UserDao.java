7a7b4da32f574435f453f2dddd1926217ed76396
potatolylc <superpotato@163.com>
2015-05-29 14:55:58 +0900

    updated comments for creating javadoc

--- src/main/java/ioedata/user/repository/UserDao.java
+++ src/main/java/ioedata/user/repository/UserDao.java
@@ -1,50 +1,73 @@
 package ioedata.user.repository;
 
 import ioedata.user.model.UserValue;
-
+/**
+ * This interface provides abstract methods specifically for user to access Oracle database.
+ * It will be implemented by UserDaoImpl class. 
+ * @author ajou
+ *
+ */
 public interface UserDao {
-	/*
-	 * Insert user into database
+	/**
+	 * Insert user into database.
+	 * @param userVal
+	 * @return
 	 */
 	public int insertUser(UserValue userVal);
 	
-	/*
-	 * Get the number user by user serial number
+	/**
+	 * Get the number user by user serial number.
+	 * @param userSerialNum
+	 * @return
 	 */
 	public int getUserCountByUserSerialNum(int userSerialNum);
 	
-	/*
-	 * Get the number of user by user name
+	/**
+	 * Get the number of user by user name.
+	 * @param userName
+	 * @return
 	 */
 	public int getUserCountByUserName(String userName);
 	
-	/*
-	 * Get the number of user by user name and WiFi SSID
+	/**
+	 * Get the number of user by user name and WiFi SSID.
+	 * @param userVal
+	 * @return
 	 */
 	public int getUserCountByUserNameAndSsid(UserValue userVal);
 	
-	/*
+	/**
 	 * Get the number of user by user name, WiFi SSID and user password.
+	 * @param userVal
+	 * @return
 	 */
 	public int getUserCountByUserNameAndSsidAndPassword(UserValue userVal);
 	
-	/*
+	/**
 	 * Get the number of user by user name and WiFi SSID.
+	 * @param userVal
+	 * @return
 	 */
 	public int getUserSerialNumByUserNameAndSsid(UserValue userVal);
 	
-	/*
+	/**
 	 * Get the unique user serial number of user by user name, WiFi SSID and user password.
+	 * @param userVal
+	 * @return
 	 */
 	public int getUserSerialNumByUserNameAndSsidAndPassword(UserValue userVal);
 	
-	/*
+	/**
 	 * Get user information by user name and WiFi SSID.
+	 * @param userSerialNum
+	 * @return
 	 */
 	public UserValue getUserInfoByUserSerialNum(int userSerialNum);
 	
-	/*
+	/**
 	 * Get user information by user name and WiFi SSID.
+	 * @param userVal
+	 * @return
 	 */
 	public UserValue getUserInfoByUserNameAndSsid(UserValue userVal);
 }
--- src/main/java/ioedata/user/service/UserService.java
+++ src/main/java/ioedata/user/service/UserService.java
@@ -15,49 +15,72 @@ import ioedata.user.model.UserValue;
  * 
  */
 public interface UserService {
-	/*
+	/**
 	 * Register new users. Before registration, it first check whether the user
 	 * name has existed in the database; if so, go on checking whether the user
 	 * has registered with the same WiFi SSID. If there isn't a user registered
 	 * with the user name and the SSID, then register and return the result.
+	 * @param userVal
+	 * @return
+	 * @throws JSONException
+	 * @throws JoseException
+	 * @throws UserDuplicateException
 	 */
 	public boolean registerUser(UserValue userVal) throws JSONException,
 			JoseException, UserDuplicateException;
 
-	/*
+	/**
 	 * Authenticate a user. 
 	 * If user name, user WiFi SSID and password matches simultaneously,
 	 * return true; else, return false.
+	 * @param userVal
+	 * @return
+	 * @throws UserNotExistException
 	 */
 	public boolean authenticateUser(UserValue userVal) throws UserNotExistException;
 	
-	/*
+	/**
 	 * Get a user serial num with user name and WiFi SSID.
+	 * @param userVal
+	 * @return
 	 */
 	public int retrieveUserSerialNumByUserNameAndSsid(UserValue userVal);
 	
-	/*
+	/**
 	 * Get a user serial num with user name, WiFi SSID and password.
+	 * @param userVal
+	 * @return
 	 */
 	public int retrieveUserSerialNumByUserNameAndSsidAndPassword(UserValue userVal);
 	
-	/*
+	/**
 	 * Get user information using user name and user WiFi SSID.
+	 * @param userSerialNum
+	 * @return
+	 * @throws UserNotExistException
 	 */
 	public UserValue retrieveUserInfoByUserSerialNum(int userSerialNum) throws UserNotExistException;
 	
-	/*
+	/**
 	 * Get user information using user name and user WiFi SSID.
+	 * @param userVal
+	 * @return
+	 * @throws UserNotExistException
 	 */
 	public UserValue retrieveUserInfoByUserNameAndSsid(UserValue userVal) throws UserNotExistException;
 	
-	/*
+	/**
 	 * Check whether a user exists by user serial number.
+	 * @param userSerialNum
+	 * @return
 	 */
 	public boolean isUserExist(int userSerialNum);
 	
-	/*
+	/**
 	 * Check whether a user exists by user name and WiFi SSID.
+	 * @param userName
+	 * @param userWifiSsid
+	 * @return
 	 */
 	public boolean isUserExist(String userName, String userWifiSsid);
 }
